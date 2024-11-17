d412e5ba1bf28f4dd390fad19e21f6d6cd1e8039
Daud gul <codrdaud@gmail.com>
2024-11-17 11:10:08 +0530

    update code

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [data, setData] = useState([]);
+
+  const fetchData = async () => {
+    try {
+      const res = await fetch(
+        "https://xcountries-backend.azurewebsites.net/all"
+      );
+      if (!res.ok) {
+        alert("there is some issue in the json");
+      }
+      const result = await res.json();
+      setData(result);
+    } catch (error) {
+      alert(error.message);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+  console.log(data, "newDAta");
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {data &&
+        data.map((item, index) => (
+          <Card key={index} name={item.name} url={item.flag} />
+        ))}
     </div>
   );
 }
 
 export default App;
+
+function Card({ name, url }) {
+  return (
+    <div className="card_body">
+      <img src={url} alt={name} />
+      <h2>{name}</h2>
+    </div>
+  );
+}
